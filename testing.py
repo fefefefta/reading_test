91876592e195ff3ad2329f6be08fd5ed914277dc
fefta <stuffefta@gmail.com>
2021-08-28 23:36:27 +0300

    скелет проекта

---
+++ testing.py
@@ -0,0 +1,16 @@
+
+
+class Testing:
+
+	def __init__(self):
+		self.text_info = get_text_info()
+
+
+	def get_text_info(self):
+		pass
+
+
+	def reading_test(self):
+		pass
+		#return self.reading_score
+
---
+++ user.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+class User:
+
+	def __init__(self):
+		self.__name = self.auth()
+
+
+	def auth(login, password):
+		pass
+
+
+	def get_name():
+		return self.__name
+
+
+	def reg(login, password):
+		pass
